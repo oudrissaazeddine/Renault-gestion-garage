e1a0cb9f4b610e5fd552f67166885833efee247a
oudriss aazeddine <oudriss.aazeddine@gmail.com>
2026-02-23 22:18:04 +0000

    spring initial

---
+++ src/main/java/com/oudriss/Renault_gestion_garage/RenaultGestionGarageApplication.java
@@ -0,0 +1,13 @@
+package com.oudriss.Renault_gestion_garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenaultGestionGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenaultGestionGarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oudriss/Renault_gestion_garage/RenaultGestionGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oudriss.Renault_gestion_garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenaultGestionGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
